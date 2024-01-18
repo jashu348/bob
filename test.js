0ab4adb3d4e0d1a2cdc9d0cc2352321e1b2b0b7f
jashu348 <jashukumar@live.com>
2024-01-18 12:31:13 +0530

    third commit(add functionality implemented)

--- test.js
+++ test.js
@@ -9,3 +9,9 @@ let sum = num1 + num2;
 console.log("Sum :", sum);
    
 }
+let addition = (a, b) => a + b;
+ 
+let num1 = 25;
+let num2 = 25;
+let sum = addition(num1, num2);
+console.log("Sum of given numbers is :", sum);
