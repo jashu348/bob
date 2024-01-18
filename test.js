be5d39a0879f76fdabcd51b6ab7aa638abba4d46
jashu348 <jashukumar@live.com>
2024-01-18 12:15:29 +0530

    third commit(add functionality implemented)

--- test.js
+++ test.js
@@ -3,5 +3,9 @@ for (let i= 0; i<=5; i++) {
     const a=23;
     console.log("Welcome"+a)
     console.log("First Change")
+    let num1 = 10;
+let num2 = 10;
+let sum = num1 + num2;
+console.log("Sum :", sum);
    
 }
